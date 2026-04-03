49994af58710b173593b046583b53c48e28c1e65
azip7777 <jeyone228@mail.ru>
2026-04-03 20:22:37 +0300

    Update sync-all-movies.js

--- sync-all-movies.js
+++ sync-all-movies.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 // Replace 'YOUR_BEARER_TOKEN' with your actual TMDB API Bearer Token
 const API_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MjhkYzU3NDU5NjZhYjI2OGJmMDJmNWFmZjlhYWFiYiIsIm5iZiI6MTczNTk5MjkwMi42NzkwMDAxLCJzdWIiOiI2Nzc5MjY0NjZkN2NhMDBlNzg3MjdmMGIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Huzv7cIZRRKKLNSvbRZDQZmX9w0Ny1Gif6G8LI2WW30';
-const BASE_URL = 'https://api.themoviedb.org/3';
+const BASE_URL = 'https://api.themoviedb.org/3/authentication';
 
 const syncAllMovies = async () => {
     try {
